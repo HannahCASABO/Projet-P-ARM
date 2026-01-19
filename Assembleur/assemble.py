5c8f8fdfec9e28c635e17f10ea34014e3fdaeb74
Marius-Fenniche <marius.fenniche@etu.univ-cotedazur.fr>
2026-01-19 13:33:25 +0100

    feat: Rassemblement de l'assembleur pour générer un fichier .bin au format logisim avec la traduction comme attendu

--- Assembleur/assemble.py
+++ Assembleur/assemble.py
@@ -64,24 +64,64 @@ def assemble_file(filename: str) -> list[str]:
 
     return hexadecimal
 
+from parser import parse_file, parse_file_list
+from encodeur import encode_line, list_4x4_insertion
 
 
-# =======================
-# Affichage / exécution
-# =======================
+def encode_program_bin(program: list[dict]) -> list[int]:
+    """
+    Encode tout le programme avec un affichage en 4x4 bits
 
-program = parse_file("unitar.txt")
-print_parseur = parse_file_list("branch.txt")
+    => Renvoie une liste d'entiers 16 bits
+    """
+    out = []
+    for entry in program:
+        if entry["type"] == "instruction":
+            tokens = [entry["mnemonic"]] + entry["operands"]
+            sequence = encode_line(tokens)   # int 16 bits
+            out.append(sequence)
 
-print("Retour du parseur :")
-for inst in print_parseur:
-    print(inst)
+            # affichage binaire conservé
+            print(f"{tokens} -> {' '.join(list_4x4_insertion(sequence))}")
 
-print("\nEncodage:")
-binary = encode_program_bin(program)
+    print("\nAffichage de la liste d'encodage du fichier :")
+    print(out)
+    return out
 
-print("\nConversion hexadecimal:")
-hexadecimal = convert_hexadecimal(binary)
 
-print("\nRésultat final :")
-print(" ".join(hexadecimal))
+def convert_hexadecimal(binary: list[int]) -> list[str]:
+    """
+    Convertit une liste d'entiers 16 bits en hexadécimal (base 16)
+    """
+    hex_out = []
+    for word in binary:
+        hex_out.append(f"{word:04x}")
+
+    print(hex_out)
+    return hex_out
+
+
+def assemble_file(filename: str) -> list[str]:
+    """
+    Pipeline complet assembleur :
+    asm -> binaire -> hexadécimal
+
+    Retourne la liste hexadécimale
+    """
+    program = parse_file(filename)
+    print_parseur = parse_file_list(filename)
+
+    print("Retour du parseur :")
+    for inst in print_parseur:
+        print(inst)
+
+    print("\nEncodage:")
+    binary = encode_program_bin(program)
+
+    print("\nConversion hexadecimal:")
+    hexadecimal = convert_hexadecimal(binary)
+
+    print("\nRésultat final :")
+    print(" ".join(hexadecimal))
+
+    return hexadecimal
